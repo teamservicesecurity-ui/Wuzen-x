ed358aab25a44876b20f582596af1607768c1f2f
teamservicesecurity-ui <teamservicesecurity@gmail.com>
2026-07-27 00:04:25 +0100

    Update build.gradle.kts

--- android-client/build.gradle.kts
+++ android-client/build.gradle.kts
@@ -1,34 +1,4 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.example.wuzenx"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.wuzenx"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    // NEW Kotlin 2.x Compiler Options DSL
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
